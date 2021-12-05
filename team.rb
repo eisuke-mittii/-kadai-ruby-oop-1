e13437e9ee02ad334574e0a2f7a901abfab61ccc
eisuke.mitsu <m.eisuke1229@gmail.com>
2021-12-05 15:46:00 +0000

    modified_initialize

--- team.rb
+++ team.rb
@@ -1,11 +1,11 @@
 class Team
     attr_accessor :name, :win, :lose, :draw
     
-    def initialize
-        self.name = "Giants" 
-        self.win = 67
-        self.lose = 45
-        self.draw = 8
+    def initialize(name, win, lose, draw)
+        self.name = name
+        self.win = win
+        self.lose = lose
+        self.draw = draw
     end
         
     def calc_win_rate
@@ -17,37 +17,17 @@ class Team
     end
 end
 
-team1 = Team.new
+team1 = Team.new("Giants", 67, 45, 8)
 
-team2 = Team.new
-team2.name = "Tigers"
-team2.win = 60
-team2.lose = 53
-team2.draw = 7
+team2 = Team.new("Tigers", 60, 53, 7)
 
-team3 = Team.new
-team3.name = "Drgons"
-team3.win = 60
-team3.lose = 55
-team3.draw = 5
+team3 = Team.new("Dragons", 60, 55, 5)
 
-team4 = Team.new
-team4.name = "BayStars"
-team4.win = 56
-team4.lose = 58
-team4.draw = 6
+team4 = Team.new("BayStars", 56, 58, 6)
 
-team5 = Team.new
-team5.name = "Carp"
-team5.win = 52
-team5.lose = 56
-team5.draw = 12
+team5 = Team.new("Carp", 52, 56, 12)
 
-team6 = Team.new
-team6.name = "Swallows"
-team6.win = 41
-team6.lose = 69
-team6.draw = 10
+team6 = Team.new("Swallows", 41, 69, 10)
 
 team1.show_team_result
 team2.show_team_result
